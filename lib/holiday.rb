9d6f72e95f643db02bbb3a962dc451e7a8fb17d0
Virgil Cantu <cantu.sampsom@gmail.com>
2020-08-02 05:02:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,5 +63,7 @@ def all_holidays_with_bbq(holiday_hash)
    binding.pry
    holidays.each do
 end
+end
+end
 # return an array of holiday names (as symbols) where supply lists
 # include the string "BBQ"
